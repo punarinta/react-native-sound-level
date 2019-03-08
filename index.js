ccbd78c391c70dae9a756b49478d1164eced06e3
Kammeh <phil@pwiltsher.com>
2019-03-08 00:11:42 +0000

    Add custom monitor interval for frames on iOS.

--- index.js
+++ index.js
@@ -1,38 +1,42 @@
-'use strict';
+'use strict'
 
-import { NativeModules, NativeAppEventEmitter, Platform } from "react-native";
+import { NativeModules, NativeAppEventEmitter, Platform } from 'react-native'
 
-var SoundLevelModule = Platform.OS === 'desktop' ? NativeModules.RNSoundLevel : NativeModules.RNSoundLevelModule;
+var SoundLevelModule =
+  Platform.OS === 'desktop'
+    ? NativeModules.RNSoundLevel
+    : NativeModules.RNSoundLevelModule
 
 var SoundLevel = {
   timer: null,
-  start: function() {
-    if (this.frameSubscription) this.frameSubscription.remove();
+  start: function (monitorInterval) {
+    if (this.frameSubscription) this.frameSubscription.remove()
 
     if (Platform.OS === 'desktop') {
       this.timer = setInterval(async () => {
         if (this.onNewFrame) {
           const frame = await SoundLevelModule.measure()
-          this.onNewFrame(JSON.parse(frame));
+          this.onNewFrame(JSON.parse(frame))
         }
       }, 250)
     } else {
-      this.frameSubscription = NativeAppEventEmitter.addListener('frame',
-        (data) => {
+      this.frameSubscription = NativeAppEventEmitter.addListener(
+        'frame',
+        data => {
           if (this.onNewFrame) {
-            this.onNewFrame(data);
+            this.onNewFrame(data)
           }
         }
-      );
+      )
     }
 
-    return SoundLevelModule.start();
+    return SoundLevelModule.start(monitorInterval)
   },
-  stop: function() {
-    if (this.frameSubscription) this.frameSubscription.remove();
-    if (this.timer) clearInterval(this.timer);
-    return SoundLevelModule.stop();
-  },
-};
+  stop: function () {
+    if (this.frameSubscription) this.frameSubscription.remove()
+    if (this.timer) clearInterval(this.timer)
+    return SoundLevelModule.stop()
+  }
+}
 
-module.exports = SoundLevel;
+module.exports = SoundLevel
