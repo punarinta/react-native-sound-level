2b20311dd267a9d20ba28dc95e461661bb536040
yasith <yasith@cs.odu.edu>
2019-07-12 19:09:04 -0400

    Remove AppCompat Dependency Update build.gradle to compile to new SDK versions Refactor Java

--- android/src/main/java/com/punarinta/RNSoundLevel/RNSoundLevel.java
+++ android/src/main/java/com/punarinta/RNSoundLevel/RNSoundLevel.java
@@ -12,25 +12,20 @@ import java.util.List;
 
 public class RNSoundLevel implements ReactPackage {
 
-    /**
-     * @param reactContext react application context that can be used to create modules
-     * @return list of native modules to register with the newly created catalyst instance
-     */
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
         List<NativeModule> modules = new ArrayList<>();
-
         modules.add(new RNSoundLevelModule(reactContext));
-
         return modules;
     }
 
-    /**
-     * @param reactContext
-     * @return a list of view managers that should be registered with {UIManagerModule}
-     */
+    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-        return Collections.emptyList();
+        return null;
     }
 }
